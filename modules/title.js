44e950bf5cb9f80515832e5fbef12cf84aed03fe
rbarax <81901800+rbarax@users.noreply.github.com>
2024-02-05 20:57:35 +0300

    Update title.js

--- modules/title.js
+++ modules/title.js
@@ -2,7 +2,7 @@ function console_title(x) {
     if (process.platform == 'win32') {
         process.title = x + " / smsbomber - discord.gg/zara";
     } else {
-        process.stdout.write('\x1b]2;' + x + " / Sms Bomber v2.0 - illegalplatform.com" + '\x1b\x5c');
+        process.stdout.write('\x1b]2;' + x + " / Sms Bomber v2.0" + '\x1b\x5c');
     }
 }
 
